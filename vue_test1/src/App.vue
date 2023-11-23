f73e72478f17a4f2c7c74b4cf5fa064785b6ccfb
jiuri <1624873920@qq.com>
2023-11-23 18:34:53 +0800

    test

---
+++ vue_test1/src/App.vue
@@ -0,0 +1,102 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import ElText from './ElTest.vue'
+import image1 from './assets/6.jpg'
+import image3 from './components/images/10.png'
+import MeTest from './MeTest.vue'
+import image2 from './components/images/8bf69b8ce619d69d9d31a4d9cdcb595d.jpeg'
+</script>
+
+<template>
+<button @click="back">返回主页</button>
+  <div v-if="elTestShow===0">
+    <div class="common-layout">
+      <el-container>
+        <el-header>基于手势的手部康复游戏</el-header>
+        <el-main>
+          <div class="image-button-wrapper">
+            <div v-for="(item, index) in items" :key="index" class="image-button-container">
+              <img :src="item.image" :alt="'Image ' + index" class="image">
+              <button class="button" @click='show'>{{ item.buttonText }}</button>
+            </div>
+          </div>
+        </el-main>
+      </el-container>
+    </div>
+  </div>
+<div v-else-if="elTestShow===1">
+  <MeTest></MeTest>
+</div>
+<!--  <button @click="show">33</button>-->
+<!--  <button @click="ss">44</button>-->
+<!--&lt;!&ndash;  <ElText></ElText>&ndash;&gt;-->
+<!--  <div class="common-layout" >-->
+<!--    <ElText v-if="elTestShow===1"></ElText>-->
+<!--  </div>-->
+<!--<div v-show="elTestShow===0">-->
+<!--  <MeTest ></MeTest>-->
+<!--</div>-->
+<!--  <MeTest></MeTest>-->
+</template>
+
+<script>
+
+export default {
+  // components:{ElTest},
+  data(){
+    return{
+      message:"eee22",
+      elTestShow:0,
+      items: [
+        { image: 'src/components/images/8bf69b8ce619d69d9d31a4d9cdcb595d.jpeg', buttonText: '普通模式',shijian:'h' },
+        { image: 'src/components/images/9.png', buttonText: '无尽模式',shijian:'h' },
+        { image: 'src/components/images/10.png', buttonText: '闯关模式' ,shijian:'h'},
+      ]
+    }
+  },
+methods:{
+    show(){
+      this.elTestShow=1;
+      alert(this.elTestShow)
+    },
+  ss(){
+    alert(this.elTestShow)
+  },
+  back(){
+      this.elTestShow=0;
+  }
+}
+}
+</script>
+
+
+<style scoped>
+
+.common-layout {
+  display: flex;
+  justify-content: space-around;
+}
+
+.image-button-wrapper {
+  display: flex;
+  justify-content: space-around;
+}
+
+.image-button-container {
+  width: fit-content;
+}
+
+.image {
+  width: 200px;
+  height: 200px;
+  object-fit: cover;
+}
+
+.button {
+  width: 100%;
+}
+
+
+
+</style>
---
+++ vue_test1/src/MeTest.vue
@@ -0,0 +1,374 @@
+<template>
+  <div>
+<!--    <video ref="video" autoplay playsinline muted></video>-->
+<!--    <canvas ref="canvas"></canvas>-->
+<!--    <button @click="handleClick">jui</button>-->
+
+
+
+  </div>
+
+
+
+
+
+
+
+<!--  <h1>Recognize hand gestures using the MediaPipe HandGestureRecognizer task</h1>-->
+
+  <section id="demos" class="invisible">
+<!--    <h2>Demo: Recognize gestures</h2>-->
+<!--    <p><em>Click on an image below</em> to identify the gestures in the image.</p>-->
+<!---->
+<!--    <div class="detectOnClick">-->
+<!--      <img src="https://assets.codepen.io/9177687/idea-gcbe74dc69_1920.jpg" crossorigin="anonymous" title="Click to get recognize!" />-->
+<!--      <p class="classification removed"></p>-->
+<!--    </div>-->
+<!--    <div class="detectOnClick">-->
+<!--      <img src="https://assets.codepen.io/9177687/thumbs-up-ga409ddbd6_1.png" crossorigin="anonymous" title="Click to get recognize!" />-->
+<!--      <p class="classification removed"></p>-->
+<!--    </div>-->
+
+<!--    <h2><br>Demo: Webcam continuous hand gesture detection</h2>-->
+<!--    <p>Use your hand to make gestures in front of the camera to get gesture classification. <br>Click <b>enable webcam</b> below and grant access to the webcam if prompted.</p>-->
+    <div class="image-container">
+      <img src="image1.jpg" alt="Image 1">
+      <img src="image2.jpg" alt="Image 2">
+      <img src="image3.jpg" alt="Image 3">
+      <img src="image4.jpg" alt="Image 4">
+      <img src="image5.jpg" alt="Image 5">
+    </div>
+    <div id="liveView" class="videoView">
+      <button id="webcamButton" class="mdc-button mdc-button--raised">
+        <span class="mdc-button__ripple"></span>
+
+
+
+        <span class="mdc-button__label">开始测试</span>
+      </button>
+      <div style="position: relative;">
+        <video id="webcam" ref="video" autoplay playsinline></video>
+        <canvas class="output_canvas" id="output_canvas" width="1280" height="720" style="position: absolute; left: 0px; top: 0px;"></canvas>
+        <p id='gesture_output' class="output"></p>
+      </div>
+    </div>
+  </section>
+</template>
+
+
+
+<script>
+import {
+  GestureRecognizer,
+  FilesetResolver,
+  DrawingUtils
+} from "@mediapipe/tasks-vision";
+
+export default {
+  data() {
+    return {
+      worker: null,
+      camera: null,
+      gestureRecognizer: null,
+      runningMode: "IMAGE",
+      enableWebcamButton: null,
+      webcamRunning: false,
+      videoHeight: "360px",
+      videoWidth: "480px",
+      demosSection: null,
+      lastVideoTime: -1,
+      results: undefined,
+    };
+  },
+  async mounted() {
+    this.demosSection = document.getElementById("demos");
+    this.createGestureRecognizer();
+    this.enableWebcamButton = document.getElementById("webcamButton");
+    if (this.hasGetUserMedia()) {
+      this.enableWebcamButton.addEventListener("click", this.enableCam);
+    } else {
+      console.warn("getUserMedia() is not supported by your browser");
+    }
+
+    this.$nextTick(() => {
+      this.demosSection = document.getElementById("demos");
+      this.createGestureRecognizer();
+      this.enableWebcamButton = document.getElementById("webcamButton");
+      if (this.hasGetUserMedia()) {
+        this.enableWebcamButton.addEventListener("click", this.enableCam);
+      } else {
+        console.warn("getUserMedia() is not supported by your browser");
+      }
+    });
+  },
+  methods: {
+    async createGestureRecognizer() {
+      const vision = await FilesetResolver.forVisionTasks(
+          // "@mediapipe/tasks-vision/wasm"
+          "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
+      );
+      this.gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
+        baseOptions: {
+          modelAssetPath:
+          // "",
+              "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
+          delegate: "GPU"
+        },
+        runningMode: this.runningMode
+      });
+      this.demosSection.classList.remove("invisible");
+    },
+    async handleClick(event) {
+      if (!this.gestureRecognizer) {
+        alert("Please wait for gestureRecognizer to load");
+        return;
+      }
+
+      if (this.runningMode === "VIDEO") {
+        this.runningMode = "IMAGE";
+        await this.gestureRecognizer.setOptions({ runningMode: "IMAGE" });
+      }
+      // Remove all previous landmarks
+      const allCanvas = event.target.parentNode.getElementsByClassName("canvas");
+      for (var i = allCanvas.length - 1; i >= 0; i--) {
+        const n = allCanvas[i];
+        n.parentNode.removeChild(n);
+      }
+
+      const results = this.gestureRecognizer.recognize(event.target);
+
+      // View results in the console to see their format
+      console.log(results);
+      if (results.gestures.length > 0) {
+        const p = event.target.parentNode.childNodes[3];
+        p.setAttribute("class", "info");
+
+        const categoryName = results.gestures[0][0].categoryName;
+        const categoryScore = parseFloat(
+            results.gestures[0][0].score * 100
+        ).toFixed(2);
+        const handedness = results.handednesses[0][0].displayName;
+
+        p.innerText = `GestureRecognizer: ${categoryName}\n Confidence: ${categoryScore}%\n Handedness: ${handedness}`;
+        p.style =
+            "left: 0px;" +
+            "top: " +
+            event.target.height +
+            "px; " +
+            "width: " +
+            (event.target.width - 10) +
+            "px;";
+
+        const canvas = document.createElement("canvas");
+        canvas.setAttribute("class", "canvas");
+        canvas.setAttribute("width", event.target.naturalWidth + "px");
+        canvas.setAttribute("height", event.target.naturalHeight + "px");
+        canvas.style =
+            "left: 0px;" +
+            "top: 0px;" +
+            "width: " +
+            event.target.width +
+            "px;" +
+            "height: " +
+            event.target.height +
+            "px;";
+
+        event.target.parentNode.appendChild(canvas);
+        const canvasCtx = canvas.getContext("2d");
+        const drawingUtils = new DrawingUtils(canvasCtx);
+        for (const landmarks of results.landmarks) {
+          drawingUtils.drawConnectors(
+              landmarks,
+              GestureRecognizer.HAND_CONNECTIONS,
+              {
+                color: "#00FF00",
+                lineWidth: 5
+              }
+          );
+          drawingUtils.drawLandmarks(landmarks, {
+            color: "#FF0000",
+            lineWidth: 1
+          });
+        }
+      }
+    },
+    hasGetUserMedia() {
+      return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+    },
+    async enableCam(event) {
+      if (!this.gestureRecognizer) {
+        alert("Please wait for gestureRecognizer to load");
+        return;
+      }
+
+      if (this.webcamRunning === true) {
+        this.webcamRunning = false;
+        this.enableWebcamButton.innerText = "ENABLE PREDICTIONS";
+      } else {
+        this.webcamRunning = true;
+        this.enableWebcamButton.innerText = "DISABLE PREDICTIONS";
+      }
+
+      // getUsermedia parameters.
+      const constraints = {
+        video: true
+      };
+
+      // Activate the webcam stream.
+      navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+        this.$refs.video.srcObject = stream;
+        this.$refs.video.play();
+        this.$refs.video.addEventListener("loadeddata", this.predictWebcam);
+      });
+    },
+    async predictWebcam() {
+      const webcamElement = this.$refs.video;
+      const canvasElement = this.$refs.canvas;
+      const canvasCtx = canvasElement.getContext("2d");
+
+      // Now let's start detecting the stream.
+      if (this.runningMode === "IMAGE") {
+        this.runningMode = "VIDEO";
+        await this.gestureRecognizer.setOptions({ runningMode: "VIDEO" });
+      }
+      let nowInMs = Date.now();
+      if (webcamElement.currentTime !== this.lastVideoTime) {
+        this.lastVideoTime = webcamElement.currentTime;
+        this.results = this.gestureRecognizer.recognizeForVideo(webcamElement, nowInMs);
+      }
+
+      canvasCtx.save();
+      canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+      const drawingUtils = new DrawingUtils(canvasCtx);
+
+      canvasElement.style.height = this.videoHeight;
+      webcamElement.style.height = this.videoHeight;
+      canvasElement.style.width = this.videoWidth;
+      webcamElement.style.width = this.videoWidth;
+
+      if (this.results.landmarks) {
+        for (const landmarks of this.results.landmarks) {
+          drawingUtils.drawConnectors(
+              landmarks,
+              GestureRecognizer.HAND_CONNECTIONS,
+              {
+                color: "#00FF00",
+                lineWidth: 5
+              }
+          );
+          drawingUtils.drawLandmarks(landmarks, {
+            color: "#FF0000",
+            lineWidth: 2
+          });
+        }
+      }
+      canvasCtx.restore();
+
+      // Call this function again to keep predicting when the browser is ready.
+      if (this.webcamRunning === true) {
+        window.requestAnimationFrame(this.predictWebcam);
+      }
+    }
+  },
+};
+</script>
+<style>
+@use "@material";
+body {
+  font-family: roboto;
+  margin: 2em;
+  color: #3d3d3d;
+  --mdc-theme-primary: #007f8b;
+  --mdc-theme-on-primary: #f1f3f4;
+}
+
+h1 {
+  color: #007f8b;
+}
+
+h2 {
+  clear: both;
+}
+
+video {
+  clear: both;
+  display: block;
+  transform: rotateY(180deg);
+  -webkit-transform: rotateY(180deg);
+  -moz-transform: rotateY(180deg);
+  height: 280px;
+}
+
+section {
+  opacity: 1;
+  transition: opacity 500ms ease-in-out;
+}
+
+.removed {
+  display: none;
+}
+
+.invisible {
+  opacity: 0.2;
+}
+
+.detectOnClick {
+  position: relative;
+  float: left;
+  width: 48%;
+  margin: 2% 1%;
+  cursor: pointer;
+}
+.videoView {
+  position: absolute;
+  float: left;
+  width: 48%;
+  margin: 2% 1%;
+  cursor: pointer;
+  min-height: 500px;
+}
+
+.videoView p,
+.detectOnClick p {
+  padding-top: 5px;
+  padding-bottom: 5px;
+  background-color: #007f8b;
+  color: #fff;
+  border: 1px dashed rgba(255, 255, 255, 0.7);
+  z-index: 2;
+  margin: 0;
+}
+
+.highlighter {
+  background: rgba(0, 255, 0, 0.25);
+  border: 1px dashed #fff;
+  z-index: 1;
+  position: absolute;
+}
+
+.canvas {
+  z-index: 1;
+  position: absolute;
+  pointer-events: none;
+}
+
+.output_canvas {
+  transform: rotateY(180deg);
+  -webkit-transform: rotateY(180deg);
+  -moz-transform: rotateY(180deg);
+}
+
+.detectOnClick {
+  z-index: 0;
+  font-size: calc(8px + 1.2vw);
+}
+
+.detectOnClick img {
+  width: 45vw;
+}
+.output {
+  display: none;
+  width: 100%;
+  font-size: calc(8px + 1.2vw);
+}
+</style>
