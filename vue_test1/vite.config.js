4c6d802abbc3bc8d06bff3c4169710b430fb24d3
passion616 <2868711065@qq.com>
2023-11-30 15:57:06 +0800

    前端界面第一次修改

--- vue_test1/vite.config.js
+++ vue_test1/vite.config.js
@@ -13,4 +13,16 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
+  ,build: {
+    rollupOptions: {
+      external: [
+                'src/components/images/com_4.jpeg',
+                'src/components/images/com_5.jpg',
+                'src/components/images/com_23.png',
+                'src/components/images/com_24.png',
+                'src/components/images/com_38.jpg'
+                ]
+                // 将此项添加到external数组中
+    },
+  },
 })
